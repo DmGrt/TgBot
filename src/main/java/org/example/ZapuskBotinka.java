ad3dce0473b88f06043e550d18921c3af0e94c87
DmGrt <grytsishin@gmail.com>
2021-10-18 22:42:32 +0300

    top

---
+++ src/main/java/org/example/ZapuskBotinka.java
@@ -0,0 +1,21 @@
+package org.example;
+
+import org.telegram.telegrambots.ApiContextInitializer;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+public class ZapuskBotinka {
+    public static void main(String[] args) {
+        ApiContextInitializer.init();
+
+        // Instantiate Telegram Bots API
+        TelegramBotsApi botsApi = new TelegramBotsApi();
+
+        // Register our bot
+        try {
+            botsApi.registerBot(new Botinok());
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
+}
